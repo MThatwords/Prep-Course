fd9b69f5424973eeee43449106bb3c6a24684d25
MThanCode <ricardobvil116@gmail.com>
2022-03-24 22:07:42 -0600

    finish homework html y extra

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,15 +10,34 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var array = []
+  for (const i in objeto) {
+    var temporal = []
+    temporal.push(i)
+    temporal.push(objeto[i])
+    array.push(temporal)
+  }
+  return array
 }
-
 
 function numberOfCharacters(string) {
   //La función recibe un string. Recorre el srting y devuelve el caracter con el número de veces que aparece 
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var letras = {}
+  for (var char of string){
+    for(let i = 0;i<string.length;i++){
+      if (char!=string[i]) {
+        letras[i] = char
+    }else{
+      
+    }
+  }
 }
+  return letras
+}
+console.log(numberOfCharacters("adsjfdsfsfjsdjfhacabcsbajda"));
 
 
 function capToFront(s) {
